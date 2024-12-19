99aac5d9dbb7c59f59022446079187c784d41474
coden0rth <jkrambeau@gmail.com>
2024-12-18 20:54:17 -0400

    Movement cleanup

--- SecondLifeBot/Modules/ObjectScanner.cs
+++ SecondLifeBot/Modules/ObjectScanner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using OpenMetaverse;
 
-namespace SecondLifeBot
+namespace SecondLifeBot.Modules
 {
     public class ObjectScanner
     {
